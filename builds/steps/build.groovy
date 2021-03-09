963565c180698bacc3453dc8cac586a6805dd705
Opu Zaman <ozaman@aiminspections.com>
2021-03-09 11:20:40 -0500

    fixing

--- builds/steps/build.groovy
+++ builds/steps/build.groovy
@@ -4,7 +4,7 @@ void call(){
   println "This step builds packages"
   stage("Build Step 1"){
     node('master'){
-      sh 'Some bash scripting here'
+      sh 'echo "Some bash scripting here"'
     }
   }
 
