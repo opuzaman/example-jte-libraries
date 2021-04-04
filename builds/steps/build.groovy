23e73ffa3d06a0527296b06342bf2a30c376d36d
opuzaman <ozaman@aiminspections.com>
2021-04-03 20:44:49 -0400

    Update build.groovy

--- builds/steps/build.groovy
+++ builds/steps/build.groovy
@@ -3,7 +3,7 @@
 void call(){
   println "This step builds packages"
   stage("Build Step 1"){
-    node('master'){
+    node('opu-test-jenkins-node'){
       sh 'echo "Some bash scripting here"'
     }
   }
